951ec929cb6b58fa1622d1343e5bfd4d803fa9a6
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-10-16 20:57:54 -0500

    Changing app keys

--- config/facebook.php
+++ config/facebook.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-	'app_id'          => '',
-	'api_key'         => '',
-	'secret'          => '',
+	'app_id'          => '426864947372944',
+	'api_key'         => '426864947372944',
+	'secret'          => 'e4344883df22eb139d49e9259c1c3fc2',
 );
